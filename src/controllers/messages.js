98667cd29549c35690bc11e0746d702f0fb3610e
adnanqazii <adnanqazii@users.noreply.github.com>
2020-08-24 22:21:42 +0500

    Update messages.js

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -3,10 +3,10 @@ import Model from '../models/model';
 const messagesModel = new Model('messages');
 export const messagesPage = async (req, res) => {
   try {
-    const data = await messagesModel.select('name, message');
-    res.status(200).json({ messages: data.rows });
+   // const data = await messagesModel.select('name, message');
+   return res.status(200).json({ messages: data.rows });
   } catch (err) {
-    res.status(200).json({ messages: err.stack });
+    return res.status(200).json({ messages: err.stack });
   }
 };
 export const addMessage = async (req, res) => {
